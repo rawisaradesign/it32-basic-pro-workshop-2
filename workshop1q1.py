7dabdbe21368f65836d62d1483aaa7a57da58948
Rawisara Joradon <285439080+rawisaradesign@users.noreply.github.com>
2026-07-14 16:34:43 +0700

    update

--- workshop1q1.py
+++ workshop1q1.py
@@ -2,7 +2,8 @@ quantity = int(input("จำนวนปืนที่รับมาขาย 
 cost_price = int(input("ต้นทุนของปืนที่รับมา :"))
 sell_price = int(input("ราคาที่จะนำไปขายต่อ :"))
 team_member = int(input("จำนวนลูกน้องในทีมที่ไปทำงาน :"))
-print(cost_price)
-print(sell_price)
-print(cost_price - sell_price)
-print()
+print(cost_price * quantity)#ต้นทุน
+print(sell_price * quantity)#รายรับ
+print((sell_price * quantity) - (cost_price * quantity))#กำไร
+print((sell_price * quantity) - (cost_price * quantity)  *0.2)
+print((sell_price * quantity) - (cost_price * quantity) / team_member)
