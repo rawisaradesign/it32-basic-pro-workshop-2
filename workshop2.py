696c18475e5c5807e5403721bf288198b0d6c794
Rawisara Joradon <285439080+rawisaradesign@users.noreply.github.com>
2026-07-17 22:21:23 +0700

    workshop2.py

--- workshop2.py
+++ workshop2.py
@@ -9,18 +9,22 @@ if age >= 15 and age <= 18 :
             if money >= 5000 and money <= 10000 :
                 print("ผ่าน")
                 print("ตำแหน่ง tiny")
+            else :
+                print("ไม่ผ่าน")
 if age >= 18 and age <= 25 :
     if height >= 170 and height <= 190 :
         if power >= 6 and power <= 7 :
             if money >= 10000 and money <= 50000 :
                 print("ผ่าน")
                 print("ตำแหน่ง giant")
+            else :
+                print("ไม่ผ่าน")
 if age > 25 :
     if height > 190 :
         if power > 8 :
             if money > 100000 :
                 print("ผ่าน")
                 print("ตำแหน่ง boss")
-                
-                
-
+            else :
+                print("ไม่ผ่าน")
+            
